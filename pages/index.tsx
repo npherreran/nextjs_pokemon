bfe49b1c21d416c84e0ae1cb8aca6a175aa09126
npherreran <natalia.herrera@thoughtworks.com>
2022-08-25 14:18:10 -0400

    feat: Implement React UI Library
    Signed-off-by: npherreran <natalia.herrera@thoughtworks.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 
 const HomePage: NextPage = () => {
   return (
-    <><h1>Hola mundo</h1></>
+    <>
+      <Button color="gradient">Click</Button>
+    </>
   )
 }
 
